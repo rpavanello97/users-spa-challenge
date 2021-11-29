927e28daff749f1f72cb8e7cc585e8f81712fe29
rpavanello97 <rpavanello97@hotmail.com>
2021-11-29 17:53:26 -0300

    Initial configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './features/users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  },
+  {
+    path:'users',
+    component: UsersListComponent
+  },
+  { path: '**', redirectTo: 'users' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
